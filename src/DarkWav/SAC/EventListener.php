a06f9eb560ce7a66d27abfc770608e09ffdf764e
DarkWav <simon.vogl@gmx.net>
2020-03-02 21:08:27 +0100

    Add back some necessary stuff

--- src/DarkWav/SAC/EventListener.php
+++ src/DarkWav/SAC/EventListener.php
@@ -1,6 +1,5 @@
 <?php
 declare(strict_types=1);
-
 namespace DarkWav\SAC;
 
 /*
@@ -27,66 +26,107 @@ use pocketmine\event\player\PlayerMoveEvent;
 
 class EventListener implements Listener
 {
-    public $Main;
-    public $Logger;
-    public $Server;
+  public $Main;
+  public $Logger;
+  public $Server;
 
-    public function __construct(Main $mn)
+  public function __construct(Main $mn)
+  {
+    $this->Main   = $mn;
+    $this->Logger = $mn->getServer()->getLogger();
+    $this->Server = $mn->getServer();
+  }
+  
+  public function onJoin(PlayerJoinEvent $event)
+  {
+    $plr      = $event->getPlayer();
+    $uuid     = $plr->getRawUniqueID();
+    $name     = $plr->getName();
+    $olduuid  = null;
+    $analyzer = null;
+    foreach ($this->Main->Analyzers as $key=>$ana)
     {
-        $this->Main = $mn;
-        $this->Logger = $mn->getServer()->getLogger();
-        $this->Server = $mn->getServer();
+      if ($ana->PlayerName == $name)
+      {
+        $olduuid  = $key;
+        $analyzer = $ana;
+        $analyzer->Player = $plr;
+      }
     }
-
-    public function onJoin(PlayerJoinEvent $event)
+    if ($olduuid != null)
     {
-        $player = $event->getPlayer();
-        $uuid = $player->getRawUniqueId();
-
-        $this->Main->Analyzers[$uuid] = new Analyzer($player, $this->Main);
-        $this->Main->Analyzers[$uuid]->onPlayerJoin();
+      unset($this->Main->Analyzers[$olduuid]);
+      $this->Main->Analyzers[$uuid] = $analyzer;
+      $this->Main->Analyzers[$uuid]->onPlayerRejoin();
     }
-
-    public function onQuit(PlayerQuitEvent $event)
+    else
     {
-        $player = $event->getPlayer();
-        $uuid = $player->getRawUniqueId();
-
-        unset($this->Main->Analyzers[$uuid]);
+      $analyzer = new Analyzer($plr, $this->Main);
+      $this->Main->Analyzers[$uuid] = $analyzer;
+      $this->Main->Analyzers[$uuid]->onPlayerJoin();
     }
+  }
 
-    public function onMove(PlayerMoveEvent $event)
+  public function onQuit(PlayerQuitEvent $event)
+  {
+    $plr      = $event->getPlayer();
+    $uuid     = $plr->getRawUniqueID();
+    if (!empty($plr) and !empty($uuid) and array_key_exists($uuid , $this->Main->Analyzers))
     {
-        $player = $event->getPlayer();
-        $analyzer = $this->Main->Analyzers[$player->getRawUniqueId()];
+      $analyzer = $this->Main->Analyzers[$uuid];
+      if (!empty($analyzer))
+      {
+        $analyzer->onPlayerQuit();
+      }
+      $this->Main->Analyzers[$uuid]->Player = null;
+    }
+  }
 
-        if ($analyzer !== null) {
-            $analyzer->onPlayerMoveEvent($event);
+  public function onMove(PlayerMoveEvent $event)
+  {
+    $plr      = $event->getPlayer();
+    $uuid     = $plr->getRawUniqueID();
+    if (array_key_exists($uuid , $this->Main->Analyzers))
+    {
+      if($plr != null and $this->Main->Analyzers[$uuid]->Player != null)
+      {
+        $this->Main->Analyzers[$uuid]->onPlayerMoveEvent($event);
+      }
+    }
+  }
+  
+  public function onDamage(EntityDamageEvent $event)
+  {
+    $entity       = $event->getEntity();
+    if($entity instanceof Player)
+    {
+      $entityuuid   = $entity->getRawUniqueID();
+      if (array_key_exists($entityuuid , $this->Main->Analyzers))
+      {
+        if($entity != null and $this->Main->Analyzers[$entityuuid]->Player != null)
+        {
+          $this->Main->Analyzers[$entityuuid]->onPlayerGetsHit($event);
         }
+      }
     }
-
-    public function onDamage(EntityDamageEvent $event)
+    if($event instanceof EntityDamageByEntityEvent)
     {
-        $entity = $event->getEntity();
-
-        if ($entity instanceof Player) {
-            $analyzer = $this->Main->Analyzers[$entity->getRawUniqueId()];
-
-            if ($analyzer !== null) {
-                $analyzer->onPlayerGetsHit();
+      $damager      =  $event->getDamager();
+      if($damager instanceof Player)
+      {
+        $damageruuid  =  $damager->getRawUniqueID();
+        if (array_key_exists($damageruuid , $this->Main->Analyzers))
+        {
+          if($damager != null and $this->Main->Analyzers[$damageruuid]->Player != null)
+          {
+            if ($event->getCause() == EntityDamageEvent::CAUSE_ENTITY_ATTACK)
+            {
+              $this->Main->Analyzers[$damageruuid]->onPlayerPerformsHit($event);
             }
+          }
         }
-
-        if ($event instanceof EntityDamageByEntityEvent) {
-            $damager = $event->getDamager();
-
-            if ($damager instanceof Player) {
-                $analyzer = $this->Main->Analyzers[$damager->getRawUniqueId()];
-
-                if ($analyzer !== null) {
-                    $analyzer->onPlayerPerformsHit($event);
-                }
-            }
-        }
+      }
     }
+  }
+
 }
--- src/DarkWav/SAC/Main.php
+++ src/DarkWav/SAC/Main.php
@@ -1,6 +1,5 @@
 <?php
 declare(strict_types=1);
-
 namespace DarkWav\SAC;
 
 /*
@@ -15,75 +14,110 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\utils\Config;
+
+use DarkWav\SAC\EventListener;
+use DarkWav\SAC\Analyzer;
+use DarkWav\SAC\KickTask;
 
 class Main extends PluginBase
 {
-    #global variables
-    public $Colorized;
-    public $version = "4.0.7";
-    public $config_version = "1.0.0";
-    public $logger;
-    public $server;
-    public $Config;
-    public $Analyzers = array();
+  #global variables
+  public $Colorized;
+  public $version = "4.0.7";
+  public $config_version = "1.0.0";
+  public $logger;
+  public $server;
+  public $Config;
+  public $Analyzers = array();
 
-    public function onEnable(): void
+  public function onEnable() : void
+  {
+    $this->logger = $this->getServer()->getLogger();
+    $this->server = $this->getServer();
+    $this->server->getPluginManager()->registerEvents(new EventListener($this), $this);
+    @mkdir($this->getDataFolder()); #create folder for config files, ect
+    $this->saveDefaultConfig();
+    $this->Config = $this->getConfig();
+    $this->Colorized = $this->Config->get("Color"); #set color for output messages
+    $this->logger->info("[SAC] > ShadowAntiCheat enabled");
+
+    #config integrity check
+
+    switch($this->Config->get("config_version")) #check if the config is up to date.
     {
-        $this->logger = $this->getServer()->getLogger();
-        $this->server = $this->getServer();
-        $this->Config = $this->getConfig();
-        $this->Colorized = $this->Config->get("Color"); #set color for output messages
-        @mkdir($this->getDataFolder()); #create folder for config files, ect
-        $this->saveDefaultConfig();
-        $this->server->getPluginManager()->registerEvents(new EventListener($this), $this);
-        $this->logger->info("[SAC] > ShadowAntiCheat enabled");
-
-        #config integrity check
-
-        switch ($this->Config->get("config_version")) #check if the config is up to date.
-        {
-            case $this->config_version:
-                break;
-            default:
-            {
-                $this->logger->warning(TextFormat::YELLOW . "[SAC] > Your configuration file is outdated, please update when possible"); #nofify user about outdated config
-                break;
-            }
-        }
-        switch ($this->Config->get("plugin_version")) #check if the config file is compatible with the current version of the plugin.
-        {
-            case $this->version:
-                break;
-            default:
-            {
-                $this->logger->error(TextFormat::RED . "[SAC] > Your configuration file is incompatible with this version of SAC, please delete ./plugin_data/ShadowAntiCheat/config.yml"); #throw error and nofify user about incompatible config
-                $this->server->getPluginManager()->disablePlugin($this); #disable the plugin to prevent unpreventable errors
-                break;
-            }
-        }
-
-        #analyzer management
-        #connect existing players with an analyzer instance
-
-        foreach ($this->server->getOnlinePlayers() as $player) {
-            $uuid = $player->getRawUniqueId(); // raw-uuid for easier handling
-
-            $this->Analyzers[$uuid] = new Analyzer($player, $this);
-            $this->Analyzers[$uuid]->onPlayerRejoin();
-        }
+      case $this->config_version: break;
+      default:
+      {
+        $this->logger->warning(TextFormat::YELLOW . "[SAC] > Your configuration file is outdated, please update when possible"); #nofify user about outdated config
+        break;
+      }
     }
-
-    #command handling
-
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    switch($this->Config->get("plugin_version")) #check if the config file is compatible with the current version of the plugin.
     {
-        switch ($command->getName()) #get name of entered command and test for SAC commands
-        {
-            case "sac":
-            case "shadowanticheat":
-                $sender->sendMessage(TextFormat::ESCAPE . "$this->Colorized" . "[SAC] > ShadowAntiCheat v" . $this->version . " [Dizzy Devil] by DarkWav");
-                return true;
-        }
-        return false; #do not influence the further processing of the command
+      case $this->version: break;
+      default:
+      {
+        $this->logger->error(TextFormat::RED . "[SAC] > Your configuration file is incompatible with this version of SAC, please delete ./plugin_data/ShadowAntiCheat/config.yml"); #throw error and nofify user about incompatible config
+        $this->server->getPluginManager()->disablePlugin($this); #disable the plugin to prevent unpretendable errors
+        break;
+      }
     }
+    
+    #analyzer management
+    #connect existing players with an analyzer instance
+
+    foreach($this->server->getOnlinePlayers() as $player)
+    {
+      $uuid     = $player->getgetRawUniqueID();
+      $name     = $player->getName();
+      $olduuid  = null;
+      $analyzer = null;
+      foreach ($this->Analyzers as $key=>$ana)
+      {
+        if ($ana->PlayerName == $name)
+        {
+          $olduuid          = $key;
+          $analyzer         = $ana;
+          $analyzer->Player = $player;
+        }
+      }
+      if ($olduuid != null)
+      {
+        unset($this->Analyzers[$olduuid]);
+        $this->Analyzers[$uuid] = $analyzer;
+        $this->Analyzers[$uuid]->onPlayerRejoin();
+      }
+      else
+      {
+        $analyzer = new Analyzer($player, $this);
+        $this->Analyzers[$uuid] = $analyzer;
+        $this->Analyzers[$uuid]->onPlayerJoin();
+      }
+    }
+  }
+
+  #command handling
+
+  public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool
+  {
+    switch($command->getName()) #get name of entered command and test for SAC commands
+    {
+      case "sac":
+      {
+        $sender->sendMessage(TextFormat::ESCAPE."$this->Colorized"."[SAC] > ShadowAntiCheat v".$this->version." [Dizzy Devil] by DarkWav");
+        break;
+      }
+      case "shadowanticheat":
+      {
+        $sender->sendMessage(TextFormat::ESCAPE."$this->Colorized"."[SAC] > ShadowAntiCheat v".$this->version." [Dizzy Devil] by DarkWav");
+        break;
+      }
+      default:
+      {
+        break;
+      }
+    }
+    return false; #do not influence the further processing of the command
+  }
 }
